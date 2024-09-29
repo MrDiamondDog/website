3a3417787714c9fb6dc4dcd55bd957f9baaf33bb
MrDiamondDog <84212701+MrDiamondDog@users.noreply.github.com>
2024-09-29 11:47:32 -0600

    maybe

--- src/lib/contants.ts
+++ src/lib/contants.ts
@@ -4,5 +4,5 @@ export const devUrl = "http://localhost:3000";
 export const backendProdUrl = "https://server.mrdiamond.is-a.dev";
 export const backendDevUrl = "http://localhost:8000";
 
-export const wsProdUrl = "wss://server.mrdiamond.is-a.dev/ws";
+export const wsProdUrl = "wss://server.mrdiamond.is-a.dev/ws/";
 export const wsDevUrl = "ws://localhost:8080";
